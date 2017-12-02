229205221ce43fbc9aae00ca6591d204585f7773
Lukáš Kříž <lukasojd@gmail.com>
2017-12-02 02:19:03 +0100

    connect on backend api

--- tipplugin/app.js
+++ tipplugin/app.js
@@ -6,11 +6,17 @@ $(document).ready(function () {
     {
         //todo: post backend id product
         //todo: backend return ( image, url, motivationText )
-        var url = "https://www.mall.cz/kavove-kapsle/krups-3x-baleni-kapsli-nescafe-espresso";
-        var motivationText = "Káva jak ji máte rádi";
-        var price = "▶ 319 Kč";
-        var image = "/i/40627814/165/145/";
-        addSmallBox(url, image, motivationText, price);
+        (function() {
+            var apiUrl = "https://api.logoman.cz/api/test";
+
+            $.getJSON( apiUrl, {
+                //format: "json"
+            })
+                .done(function( data ) {
+                    addSmallBox(data.url, data.image, data.motivationText, data.price);
+                });
+        })();
+
     }
 
 });
@@ -19,7 +25,7 @@ function addSmallBox(url, image, motivationText, price) {
 
     var box = "<ul class=\"promo-boxes-wrapper\" style=\"margin-top: 10px\">"+
     "<li class=\"promo-box\">"+
-        "<a analytics-label=\"931819\" analytics-event=\"Box1\" analytics-category=\"TV - kampaň\" analytics-on=\"click\" style=\"background-image: url("+image+")\" class=\"promo-box-head\" href=\""+url+"\"></a>"+
+        "<a style=\"background-image: url("+image+")\" class=\"promo-box-head\" href=\""+url+"\"></a>"+
         "<a class=\"link--primary promo-box-title\" style=\"font-color:black;line-height: 1.4; letter-spacing: -0.01em; font-size: 1.1em;\" analytics-label=\"931819\" analytics-event=\"Box1\" analytics-category=\"TV - kampaň\" analytics-on=\"click\" href=\""+url+"\">"+
         "<span class=\"link--secondary\" style=\"text-decoration:underline\">"+motivationText+
     "</span>"+
@@ -28,3 +34,4 @@ function addSmallBox(url, image, motivationText, price) {
 
     $('form .detail-buttons').after(box);
 }
+
